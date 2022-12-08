45a157651adb58c1e5dbe692cdaf22a20bb3f08f
SergeyKaraman <karamans7997@gmail.com>
2022-12-08 18:53:10 +0300

    Урок 2. Базовые алгоритмы
    Урок 2. Базовые алгоритмы

--- Задача 13/Program.cs
+++ Задача 13/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Задача 13: Напишите программу, которая выводит 
 // третью цифру заданного числа или сообщает, что третьей цифры нет. 
-// Выполнить с помощью числовых операций.
 
 // 645 -> 5
 // 78 -> третьей цифры нет
@@ -8,11 +7,15 @@
 
 Console.Write("Введи число: ");
 int anyNumber = Convert.ToInt32(Console.ReadLine());
-string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2)
-{
-  Console.WriteLine("Третья цифра -> "+anyNumberText[2]);
-}
-else {
-  Console.WriteLine("-> Третьей цифры нет");
-}
+  if (anyNumber<100)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+        while (anyNumber>999)
+        {
+            anyNumber /= 10;
+        }
+        Console.WriteLine($"третья цифра: {anyNumber%10} ");
+    }
